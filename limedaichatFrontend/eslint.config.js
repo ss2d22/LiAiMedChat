6015baa1722d779495b52e419c498fc88df6491d
ss2d22 <ss2d22@soton.ac.uk>
2024-08-31 02:24:24 +0100

    installed eslint-plugin-react and updated eslint config

--- limedaichatFrontend/eslint.config.js
+++ limedaichatFrontend/eslint.config.js
@@ -3,8 +3,23 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
+  {
+    // Set the react version
+    settings: { react: { version: "18.3" } },
+    plugins: {
+      // Add the react plugin
+      react,
+    },
+    rules: {
+      // other rules...
+      // Enable its recommended rules
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+    },
+  },
   { ignores: ["dist"] },
   {
     extends: [
