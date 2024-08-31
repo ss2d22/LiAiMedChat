dccbc0ab6aa25ca83d854c2e56b7a59b5f3aba64
ss2d22 <ss2d22@soton.ac.uk>
2024-08-31 18:09:28 +0100

    added use state for email, password, and enter your password again password

--- limedaichatFrontend/src/pages/authentication/index.tsx
+++ limedaichatFrontend/src/pages/authentication/index.tsx
@@ -1,7 +1,11 @@
 import { books } from "@/assets";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
       <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
