6762f1e0491237a90becd43bad789cca1e4a9a0e
ss2d22 <ss2d22@soton.ac.uk>
2024-08-31 03:12:35 +0100

    added wildcard route that navigates back to auth for now, added /chat route pointing to the component with the boilerplate code

--- limedaichatFrontend/src/App.tsx
+++ limedaichatFrontend/src/App.tsx
@@ -1,12 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "@/App.css";
 import Auth from "@/pages/authentication";
+import Chat from "@/pages/chat";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/authentication" element={<Auth />} />
+        {/* TODO: wrap chat and any future routes that needs to be protected in suspense after the Auth is setup */}
+        <Route path="/chat" element={<Chat />} />
+        {/* TODO: add a page not found component with butten to redirect back to auth or chat home page based on if user is authenticated or not */}
+        <Route path="*" element={<Navigate to={"/authentication"} />} />
       </Routes>
     </BrowserRouter>
   );
