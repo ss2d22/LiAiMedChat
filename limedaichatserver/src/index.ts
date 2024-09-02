6e931e8e015799acf29832a5b65835dcdb4132c3
ss2d22 <ss2d22@soton.ac.uk>
2024-09-02 03:13:23 +0100

    added dev dependency for types for cors and cookie-parser , updated .env.example

--- limedaichatserver/src/index.ts
+++ limedaichatserver/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
 // setup server
 dotenv.config();
@@ -7,6 +10,7 @@ const app = express();
 
 //setup port
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGODB_URI;
 
 //hello world example
 app.get("/", (request: Request, response: Response) => {
