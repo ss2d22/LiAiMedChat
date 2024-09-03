fa445b540eafd7c117dc87804518578eb96f8978
ss2d22 <ss2d22@soton.ac.uk>
2024-09-03 10:36:14 +0100

    added: added route in entry file and added documentation for routes will with needed types
    - made the app use the /authentication route that points to the authetication routes file
    
    - added documentation using JSDocs in the authenticationRoutes File
    
    -  added missing types in the authentication routes file

--- limedaichatserver/src/routes/AuthenticationRoutes.ts
+++ limedaichatserver/src/routes/AuthenticationRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import dotenv from "dotenv";
 import { signUp } from "@/controllers/AuthenticationController";
+
 dotenv.config();
 
-const authenticationRoutes = Router();
+/**
+ * authenticationRoutes is an instance of express router that is used to setup authentication routes
+ * @author Sriram Sundar
+ *
+ * @type {Router}
+ */
+const authenticationRoutes: Router = Router();
 
 authenticationRoutes.post("/signup", signUp);
 
