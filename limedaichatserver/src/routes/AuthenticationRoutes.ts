66581f8bfa4a36c5d946440e5ee190e9041ad56f
ss2d22 <ss2d22@soton.ac.uk>
2024-09-03 10:30:53 +0100

    added: added /signup route
    - added /signup route with the signup function from the authentication controller
    
    allows the /signup method to be called once the /authentication route is added in entry file

--- limedaichatserver/src/routes/AuthenticationRoutes.ts
+++ limedaichatserver/src/routes/AuthenticationRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import dotenv from "dotenv";
-
+import { signUp } from "@/controllers/AuthenticationController";
 dotenv.config();
 
-
 const authenticationRoutes = Router();
+
+authenticationRoutes.post("/signup", signUp);
 
 export default authenticationRoutes;
