28240739ba92bcb8ab5c05e08336767758ac6ff8
ss2d22 <ss2d22@soton.ac.uk>
2024-09-03 05:21:52 +0100

    updated internal server error to chinese, deconstructed email and password from the request body, added check to see if either email or password is missing and if they are an error message of "email and password are required is sent in chinese"

--- limedaichatserver/src/controllers/AuthenticationController.ts
+++ limedaichatserver/src/controllers/AuthenticationController.ts
@@ -2,8 +2,12 @@ import { Request, Response } from "express";
 
 export const signUp = async (request: Request, response: Response, next) => {
   try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.status(400).send("需要电子邮件和密码");
+    }
   } catch (error) {
     console.error(error);
-    return response.status(500).send("internal server error");
+    return response.status(500).send("服务器内部错误");
   }
 };
