ddf6ba7407f2ea7ed4e21b709788e8f57f239716
ss2d22 <ss2d22@soton.ac.uk>
2024-09-04 09:43:56 +0100

    added JSDocs

--- limedaichatFrontend/src/state/api.ts
+++ limedaichatFrontend/src/state/api.ts
@@ -2,6 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BACKEND_URL, SIGNUP_ROUTE } from "@/utils/constants";
 
 //TODO: update payload config and body to match the server after setting up state management
+/**
+ * endpoints of apis from the backend to be used in the frontend using react redux
+ * @author Sriram Sundar
+ *
+ * @type {*}
+ */
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BACKEND_URL }),
   reducerPath: "main",
@@ -17,4 +23,10 @@ export const api = createApi({
   }),
 });
 
+/**
+ * hook to use the postSignUp mutation from react redux
+ * @author Sriram Sundar
+ *
+ * @type {*}
+ */
 export const { usePostSignUpMutation } = api;
