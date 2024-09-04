91af300bb9ccda1bbcb11c1105206ec27517c265
ss2d22 <ss2d22@soton.ac.uk>
2024-09-04 09:10:15 +0100

    added: added code for /signup endpoint
    - used createApi and fetchBaseQuery to condtruct endppoint for /api/authentication/signup from our backend
    
    - exported the usePostSignUpMutation
    
    allows signup endpoint to be called as needed using the exported hook

--- limedaichatFrontend/src/state/api.ts
+++ limedaichatFrontend/src/state/api.ts
@@ -1,2 +1,19 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//TODO: update payload config and body to match the server after setting up state management
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: [],
+  endpoints: (build) => ({
+    postSignUp: build.mutation({
+      query: (payload) => ({
+        url: "/api/authentication/signup",
+        method: "POST",
+        body: payload,
+      }),
+    }),
+  }),
+});
+
+export const { usePostSignUpMutation } = api;
