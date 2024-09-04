66a231de447912427019b99e81e8055c67e47786
ss2d22 <ss2d22@soton.ac.uk>
2024-09-04 09:21:56 +0100

    REFACTOR:refactred url and routes
    - refacted backend uel , base auth endpoint, and sign up endpoint to the to constants file
    
    allows constants to be exported from one place and for code to be neater and prevents rewriting the base for each route as template strings can be used

--- limedaichatFrontend/src/state/api.ts
+++ limedaichatFrontend/src/state/api.ts
@@ -1,14 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BACKEND_URL, SIGNUP_ROUTE } from "@/utils/constants";
 
 //TODO: update payload config and body to match the server after setting up state management
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: BACKEND_URL }),
   reducerPath: "main",
   tagTypes: [],
   endpoints: (build) => ({
     postSignUp: build.mutation({
       query: (payload) => ({
-        url: "/api/authentication/signup",
+        url: SIGNUP_ROUTE,
         method: "POST",
         body: payload,
       }),
