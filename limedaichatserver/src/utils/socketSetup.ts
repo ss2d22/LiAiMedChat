d88293d0879b98d101463971b33c6853b3e086c0
ss2d22 <ss2d22@soton.ac.uk>
2024-09-21 23:51:02 +0100

    installed and setup socket in the server
    - installed socket.io and set it up in @/utils/socketSetup
    
    - used the exported socketSetup to set up the socket in the index file
    
    - added JSDocs and relevany TODOs

---
+++ limedaichatserver/src/utils/socketSetup.ts
@@ -0,0 +1,54 @@
+import { Socket, Server as SocketIOServer } from "socket.io";
+import { Server as HttpServer } from "http";
+import { Application } from "express";
+
+/**
+ * socketSetup is a function that sets up the socket server and
+ * returns the socketIoServer
+ * @author Sriram Sundar
+ *
+ * @param {Application} app
+ * @returns {SocketIOServer}
+ */
+const socketSetup = (app: Application): SocketIOServer => {
+  const httpServer: HttpServer = new HttpServer(app);
+
+  const io: SocketIOServer = new SocketIOServer(httpServer, {
+    cors: {
+      origin: process.env.FRONT_ORIGIN as string,
+      //TODO: update methods to only include what is used after completion
+      methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+      credentials: true,
+    },
+  });
+
+  const userSocketMap: Map<string, string> = new Map();
+
+  const disconnectUser = (socket: Socket) => {
+    console.log(`User ${socket.id} disconnected`);
+    for (const [userId, socketId] of userSocketMap.entries()) {
+      if (socketId === socket.id) {
+        userSocketMap.delete(userId);
+        break;
+      }
+    }
+  };
+
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+    const userId = socket.handshake.query.userId as string;
+    if (userId) {
+      userSocketMap.set(userId, socket.id);
+      console.log(`User ${userId} connected`);
+    } else {
+      console.error("User not connected as id not provided");
+    }
+
+    socket.on("disconnect", () => disconnectUser(socket));
+  });
+  console.log("Socket server setup");
+
+  return io;
+};
+
+export default socketSetup;
